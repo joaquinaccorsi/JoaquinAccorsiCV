dbbe6c01d45ebdd387df444b93054267c8fb22bd
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2022-02-26 12:05:26 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 
 import './App.css';
-import Perfil from "./img/PERFIL.jpg"
+import Header from "./header"
 
 
 
 function App() {
   return (
-  <div className='header'>
-    <div className="navbar"> 
-
-    
-    <a className='navbar-content'>about</a>
-    <a className='navbar-content'>contact me</a>
-    <a className='navbar-content'>download cv</a>
-    
-    </div>
-
-    <div className='media'> <img className='perfil' src={Perfil}/></div>
-    <div className="header-title"> <h1 className='name'>Joaquín <span>Accorsi</span></h1> </div>
-    
-    <div className="about">
-      <h3 className='skills'>WEB DEVELOPER</h3>
-      <h3 className='skills'>DESIGNER</h3>
-      <h3 className='skills'>BLOCKCHAIN</h3>
-      <h3 className='skills'>CTO</h3>
-    </div>
-</div>
+    <Header/>
   );
 }
 
