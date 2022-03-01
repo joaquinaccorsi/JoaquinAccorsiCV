4518cca37846fde9c6222abedfe1f2c1acfa535f
Joaquín Accorsi <joaquinaccorsi@gmail.com>
2022-03-01 12:40:06 -0300

    stacks add

--- src/Stack.jsx
+++ src/Stack.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 
 import WebDevPic from "./img/webdeveloper.png";
+import DesignerPic from "./img/diseno-grafico.png"
+import BlockchainPic from "./img/blockchain.png"
+import CTOPic from "./img/chief.png"
 
 
 
 function Stack () {
     return (
 
-        
-<div className="FeatureContainer" id="Stack">
+  <>      
+<div className="FeatureContainerWD" id="Stack">
 
     
 
@@ -27,10 +30,71 @@ function Stack () {
         
     </div>
 
+ 
+
+
 
 </div>
 
+<div className="FeatureContainerDESIGNER">
 
+    <div className="container_image">
+         <h2 className="Feature-Title"><span>DESIGNER</span></h2> <img src={DesignerPic} alt="designer" className="WebDevPic" />
+    </div>
+
+    <div className="container_feature">
+        <h4 className="Feature-Text">adobe photoshop</h4>
+        <h4 className="Feature-Text">adobe illustrator</h4>
+        <h4 className="Feature-Text">ux/ui</h4>
+        <h4 className="Feature-Text">figma</h4>
+        <h4 className="Feature-Text">brand design</h4>
+
+
+        
+    </div>
+</div>
+
+<div className="FeatureContainerBLOCKCHAIN">
+    
+    <div className="container_image">
+        <h2 className="Feature-Title">BLOCKCHAIN</h2> <img src={BlockchainPic} alt="blockchain" className="WebDevPic" />
+    </div>
+
+    <div className="container_feature">
+
+
+        <h4 className="Feature-Text">fundamentals</h4>
+        <h4 className="Feature-Text">protocols</h4>
+        <h4 className="Feature-Text">economy</h4>
+        <h4 className="Feature-Text">solidity</h4>
+        <h4 className="Feature-Text">web3</h4>
+        <h4 className="Feature-Text">dapps</h4>
+        <h4 className="Feature-Text">nft's</h4>
+
+    </div>
+</div>
+
+<div className="FeatureContainerCTO">
+    
+    <div className="container_image">
+        <h2 className="Feature-Title"><span>CTO</span></h2> <img src={CTOPic} alt="cto" className="WebDevPic" />
+    </div>
+
+    <div className="container_feature">
+
+
+        <h4 className="Feature-Text">positive leader</h4>
+        <h4 className="Feature-Text">excelent relationship</h4>
+        <h4 className="Feature-Text">critical think</h4>
+        <h4 className="Feature-Text">research</h4>
+        <h4 className="Feature-Text">economy</h4>
+        <h4 className="Feature-Text">sales skills</h4>
+        
+
+    </div>
+</div>
+
+</>
 
     );
 }
